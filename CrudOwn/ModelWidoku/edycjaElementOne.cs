14b745f11ed75e1b77144a814f18bb5bb86b97f5
oljar <oljar@tlen.pl>
2025-05-18 12:37:32 +0200

    Dodaj pliki projektów.

---
+++ CrudOwn/ModelWidoku/edycjaElementOne.cs
@@ -0,0 +1,70 @@
+﻿using Kolory_WPF.ModelWidoku;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Input;
+
+namespace CrudOwn.ModelWidoku
+{
+
+
+
+    public class edycjaElementOne : ObservedObject
+    {
+        public elementOne model = new elementOne("");
+
+        ObservableCollection<string> mojaLista = new ObservableCollection<string>();
+
+        public string name
+        {
+            get
+            {
+                return model.name;
+            }
+            set
+            {
+                model.name = value;
+                onPropertyChanged(nameof(name));
+            }
+        }
+       
+        
+
+
+
+
+      
+        private RelayCommand dodaj;
+      
+        public ICommand Dodaj
+        {
+            get
+            {
+                if (dodaj == null)
+                {
+                    dodaj = new RelayCommand(PerformDodaj);
+                }
+
+                return dodaj;
+            }
+        }
+
+        private void PerformDodaj(object commandParameter)
+
+        {
+            
+            mojaLista.Add(model.name.ToString());
+            string zawartoscListy = string.Join("\n", mojaLista);
+            MessageBox.Show(zawartoscListy, "Zawartość ObservableCollection");
+        }
+
+
+    }
+}
+
+
